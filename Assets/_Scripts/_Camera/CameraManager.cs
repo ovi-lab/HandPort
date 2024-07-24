cd35efeb6385ee411353d171875e4775061ef0f5
Sonja Kuenzl <sok1958@thi.de>
2024-07-24 14:31:48 -0700

    display resolution control

--- Assets/_Scripts/_Camera/CameraManager.cs
+++ Assets/_Scripts/_Camera/CameraManager.cs
@@ -10,28 +10,56 @@ public enum CameraType
     RayView = 2,
     HandView = 3
 }
+
+public enum Resolution
+{
+    Res43 = 0,
+    Res169 = 1
+}
+
+public enum CameraAnchor
+{
+    HMDAnchor = 0,
+    HandAnchor = 1,
+}
 public class CameraManager : MonoBehaviour
 {
-
     public readonly Dictionary<string, CameraType> stringToEnumConverter = new Dictionary<string, CameraType>()
     {
         { "TopDownTarget", CameraType.TopDownTarget },
         { "TopDownRay", CameraType.TopDownRay },
         { "RayView", CameraType.RayView },
         { "HandView", CameraType.HandView }
+    };
+    public readonly Dictionary<string, Resolution> stringToEnumConverter1 = new Dictionary<string, Resolution>()
+    {
+        { "4:3", Resolution.Res43 },
+        { "16:9", Resolution.Res169 }
 
     };
+    public readonly Dictionary<string, CameraAnchor> stringToEnumConverter2 = new Dictionary<string, CameraAnchor>()
+    {
+        { "HMDAnchor", CameraAnchor.HMDAnchor },
+        { "HandAnchor", CameraAnchor.HandAnchor },
+
+    };
+    
 
     public static Vector3 ParabolaPosition => parabolaPosition;
     [SerializeField] private CameraType cameraDisplayType;
+    [SerializeField] public Resolution resolution;
+    [SerializeField] private CameraAnchor anchor;
     [SerializeField] private List<CameraPlacement> cameras;
     [SerializeField] private XRRayInteractor[] rayInteractors;
+
+    [SerializeField] private RenderTexture renderTexture43;
+    [SerializeField] private RenderTexture renderTexture169;
+
     
     private static Vector3 parabolaPosition;
     private Camera mainCamera;
     private static GameObject currentCameraGameObject;
-
-
+    
     private void Start()
     {
         foreach (CameraPlacement cam in cameras)
@@ -40,6 +68,24 @@ public class CameraManager : MonoBehaviour
         }
         cameras[(int)cameraDisplayType].gameObject.SetActive(true);
         rayInteractors = FindObjectsOfType<XRRayInteractor>();
+
+        if ((int)resolution == 0)
+        {
+            foreach (CameraPlacement cam in cameras)
+            {
+                Camera cameraComponent = cam.GetComponent<Camera>();
+                cameraComponent.targetTexture = renderTexture43;
+            }
+        }
+        else
+        {
+            foreach (CameraPlacement cam in cameras)
+            {
+                Camera cameraComponent = cam.GetComponent<Camera>();
+                cameraComponent.targetTexture = renderTexture169;
+            }
+        }
+        
     }
 
     private void Update()
@@ -75,5 +121,22 @@ public class CameraManager : MonoBehaviour
             cam.gameObject.SetActive(false);
         }
         cameras[(int)cameraDisplayType].gameObject.SetActive(true);
+        
+        if ((int)resolution == 0)
+        {
+            foreach (CameraPlacement cam in cameras)
+            {
+                Camera cameraComponent = cam.GetComponent<Camera>();
+                cameraComponent.targetTexture = renderTexture43;
+            }
+        }
+        else
+        {
+            foreach (CameraPlacement cam in cameras)
+            {
+                Camera cameraComponent = cam.GetComponent<Camera>();
+                cameraComponent.targetTexture = renderTexture169;
+            }
+        }
     }
 }
--- Assets/_Scripts/_Camera/WIMWidget.cs
+++ Assets/_Scripts/_Camera/WIMWidget.cs
@@ -2,53 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum CameraAnchor
-{
-    HMDAnchor = 0,
-    HandAnchor = 1,
-}
+
 public class WIMWidget : MonoBehaviour
 {
-    public readonly Dictionary<string, CameraAnchor> stringToEnumConverter = new Dictionary<string, CameraAnchor>()
-    {
-        { "HMDAnchor", CameraAnchor.HMDAnchor },
-        { "HandAnchor", CameraAnchor.HandAnchor },
-
-    };
-    
+    [SerializeField] private int anchorType;
     [SerializeField] private Transform[] anchorPoint;
-    [SerializeField] private CameraAnchor anchor;
 
     private Transform display;
     private bool isInitialized = false;
-    private float initialZPosition;
+    private CameraManager camManager;
 
     private void Start()
     {
         InitializeDisplay();
         if (anchorPoint.Length > 0)
         {
-            SetDisplayParent(anchorPoint[(int)anchor]);
-            // if (anchor == CameraAnchor.HandAnchor)
-            // {
-            //     initialZPosition = display.localPosition.z;
-            //     if (anchor == CameraAnchor.HandAnchor)
-            //     {
-            //         initialZPosition = display.localPosition.z;
-            //     }
-            // }
+            SetDisplayParent(anchorPoint[0]);
         }
         isInitialized = true;
-    }
-    
-    private void Update()
-    {
-        // if (anchor == CameraAnchor.HandAnchor && display != null)
-        // {
-        //     Vector3 newPosition = anchorPoint[(int)anchor].position;
-        //     newPosition.z = initialZPosition;
-        //     display.position = newPosition;
-        // }
     }
 
     private void OnValidate()
@@ -56,28 +27,27 @@ public class WIMWidget : MonoBehaviour
         if (!isInitialized) return;
 
         InitializeDisplay();
-        // if (anchorPoint.Length > (int)anchor && anchorPoint[(int)anchor] != null)
-        // {
-        //     SetDisplayParent(anchorPoint[(int)anchor]);
-        // }
-        // else
-        // {
-        //     Debug.LogWarning("Invalid anchorType or anchorPoint.");
-        // }
+        if (anchorPoint.Length > anchorType && anchorPoint[anchorType] != null)
+        {
+            SetDisplayParent(anchorPoint[anchorType]);
+        }
+        else
+        {
+            Debug.LogWarning("Invalid anchorType or anchorPoint.");
+        }
     }
 
     private void InitializeDisplay()
     {
+        foreach (Transform child  in transform)
+        { 
+            child.gameObject.SetActive(false);
+        }
         if (display == null)
         {
-            if (transform.childCount > 0)
-            {
-                display = transform.GetChild(0);
-            }
-            else
-            {
-                Debug.LogWarning("No child objects found in the Transform.");
-            }
+            camManager = transform.parent.GetComponent<CameraManager>();
+            display = transform.GetChild((int)camManager.resolution);
+            display.gameObject.SetActive(true);
         }
     }
 
@@ -86,24 +56,6 @@ public class WIMWidget : MonoBehaviour
         if (display != null)
         {
             display.SetParent(newParent, false);
-            //display.localScale = Vector3.one;
-            if (anchor == CameraAnchor.HandAnchor)
-            {
-                Vector3 localPosition = display.localPosition;
-                localPosition.x = 0;
-                localPosition.y = 1.5f;
-                localPosition.z = 2;
-                
-                display.localPosition = localPosition;
-            }
-            else
-            {
-                Vector3 localPosition = display.localPosition;
-                localPosition.x = 1;
-                localPosition.y = 1;
-                localPosition.z = 3.5f;
-                display.localPosition = localPosition;
-            }
         }
         else
         {
