2ef848d0b367ed53682674b21d4847db0c82d1c9
Sonja Kuenzl <sok1958@thi.de>
2024-07-25 11:00:04 -0700

    implement hand anchor

--- Assets/ControlDirection.cs
+++ Assets/ControlDirection.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class ControlDirection : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Quaternion newRotation = Quaternion.Euler(-30, 180, 180); 
     void Start()
     {
-        
+        transform.GetChild(0).localPosition = new Vector3(0, 0, -0.04f);
+        transform.GetChild(0).rotation = newRotation;
+        transform.GetChild(0).localScale = new Vector3(0.02f, 0.002f, 0.015f);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
